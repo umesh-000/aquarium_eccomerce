f65f66a33f4a0129ff10545e288049af5c31bfed
NoSignal000 <umeshprajapati20799@gmail.com>
2024-10-08 10:27:44 +0530

    setup complete

---
+++ aquarium_eccomerce/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls.static import static
+from django.urls import path, include
+from django.conf import settings
+
+urlpatterns = [
+    path('', include('myadmin.urls')),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
